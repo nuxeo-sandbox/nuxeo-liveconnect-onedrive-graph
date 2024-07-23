799473eeb9d83d26d463c620012052e4688bebbe
Michaël Vachette <5880528+michaelva@users.noreply.github.com>
2024-07-23 18:42:16 +0900

    Fix Auth

--- nuxeo-liveconnect-onedrive-graph-core/src/main/java/org/nuxeo/ecm/liveconnect/msgraph/MSGraphOAuth2ServiceProvider.java
+++ nuxeo-liveconnect-onedrive-graph-core/src/main/java/org/nuxeo/ecm/liveconnect/msgraph/MSGraphOAuth2ServiceProvider.java
@@ -20,17 +20,67 @@ package org.nuxeo.ecm.liveconnect.msgraph;
 
 import com.azure.core.credential.AccessToken;
 import com.azure.core.credential.TokenCredential;
+import com.google.api.client.auth.oauth2.AuthorizationCodeFlow;
 import com.google.api.client.auth.oauth2.Credential;
+import com.google.api.client.auth.oauth2.TokenResponse;
 import com.microsoft.graph.models.User;
 import com.microsoft.graph.serviceclient.GraphServiceClient;
+import com.microsoft.kiota.authentication.AuthenticationProvider;
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
+import org.nuxeo.ecm.core.api.NuxeoException;
 import org.nuxeo.ecm.liveconnect.core.AbstractLiveConnectOAuth2ServiceProvider;
 import reactor.core.publisher.Mono;
+
+import javax.servlet.http.HttpServletRequest;
+import java.io.IOException;
 
 public class MSGraphOAuth2ServiceProvider extends AbstractLiveConnectOAuth2ServiceProvider {
 
     private static final Logger log = LogManager.getLogger(MSGraphOAuth2ServiceProvider.class);
+
+    @Override
+    public Credential handleAuthorizationCallback(HttpServletRequest request) {
+
+        // Checking if there was an error such as the user denied access
+        String error = getError(request);
+        if (error != null) {
+            throw new NuxeoException("There was an error: \"" + error + "\".");
+        }
+
+        // Checking conditions on the "code" URL parameter
+        String code = getAuthorizationCode(request);
+        if (code == null) {
+            throw new NuxeoException("There is not code provided as QueryParam.");
+        }
+
+        try {
+            AuthorizationCodeFlow flow = getAuthorizationCodeFlow();
+
+            String redirectUri = getCallbackUrl(request);
+            TokenResponse tokenResponse = flow.newTokenRequest(code)
+                    .setScopes(getScopes().isEmpty() ? null : getScopes()) // some providers do not
+                    // support the 'scopes' param
+                    .setRedirectUri(redirectUri)
+                    .set("client_id",getClientId())
+                    .set("client_secret",getClientSecret())
+                    .execute();
+
+            // Create a unique userId to use with the credential store
+            String userId = getOrCreateServiceUser(request, tokenResponse.getAccessToken());
+
+            return flow.createAndStoreCredential(tokenResponse, userId);
+        } catch (IOException e) {
+            throw new NuxeoException("Failed to retrieve credential", e);
+        }
+    }
+
+    @Override
+    protected String getUserEmail(String accessToken) {
+        GraphServiceClient graphClient = getGraphClient(accessToken);
+        User user =  graphClient.me().get();
+        return user.getMail();
+    }
 
     protected GraphServiceClient getGraphClient(Credential credential) {
         String accessToken = credential.getAccessToken();
@@ -42,11 +92,5 @@ public class MSGraphOAuth2ServiceProvider extends AbstractLiveConnectOAuth2Servi
         return new GraphServiceClient(tokenCredential, this.getScopes().toArray(String[]::new));
     }
 
-    @Override
-    protected String getUserEmail(String accessToken) {
-        GraphServiceClient graphClient = getGraphClient(accessToken);
-        User user =  graphClient.me().get();
-        return user.getMail();
-    }
 
 }
